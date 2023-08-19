2b628b839daa85448693f4980ae1536c8fd7aad2
Nathan Smith <mcaeolusch@gmail.com>
2023-08-18 22:49:31 -0500

    fix lint error

--- src/lib/bin/rainbow.ts
+++ src/lib/bin/rainbow.ts
@@ -1,11 +1,11 @@
 export const rainbow = (args: string[]) => {
     const footers = document.getElementsByTagName('footer')[0]?.
         getElementsByTagName('pre'); //we have two footers, apply to both
-    for (let footer of footers) {
+    for (let i = 0; i < footers.length; i++) {
+        let footer = footers[i];
         if (footer.classList.contains('rainbow_text_animated'))
             footer.classList.remove('rainbow_text_animated');
         else
             footer.classList.add('rainbow_text_animated');
     }
-
 }
